13c19b6573064a1a14e65740de356976ccffd64a
Troy Whitney <42855433+IILLC@users.noreply.github.com>
2018-12-01 10:58:54 -0700

    Add files via upload

---
+++ includes/class-fyxt-gm-tools-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.imageinnovationsllc.com/
+ * @since      1.0.0
+ *
+ * @package    Fyxt_Gm_Tools
+ * @subpackage Fyxt_Gm_Tools/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Fyxt_Gm_Tools
+ * @subpackage Fyxt_Gm_Tools/includes
+ * @author     Troy Whitney <troy@imageinnovationsllc.com>
+ */
+class Fyxt_Gm_Tools_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-fyxt-gm-tools-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.imageinnovationsllc.com/
+ * @since      1.0.0
+ *
+ * @package    Fyxt_Gm_Tools
+ * @subpackage Fyxt_Gm_Tools/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Fyxt_Gm_Tools
+ * @subpackage Fyxt_Gm_Tools/includes
+ * @author     Troy Whitney <troy@imageinnovationsllc.com>
+ */
+class Fyxt_Gm_Tools_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
